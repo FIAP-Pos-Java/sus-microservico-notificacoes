f38b674814b62e0cdb6531f868027958255d79bd
eduardaclx <eduarda.evangelista@sptech.school>
2026-02-08 17:59:09 -0300

    feat: ajustando para rodar em docker compose

--- src/main/java/sus/microservico/notificacoes/sus_microservico_notificacoes/config/RabbitMQConfig.java
+++ src/main/java/sus/microservico/notificacoes/sus_microservico_notificacoes/config/RabbitMQConfig.java
@@ -1,5 +1,9 @@
 package sus.microservico.notificacoes.sus_microservico_notificacoes.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +12,56 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
     
+    public static final String EXCHANGE = "sus.exchange";
+    
     public static final String CIRURGIA_CRIADA_QUEUE = "notificacao.cirurgia.criada.queue";
     public static final String CIRURGIA_ATUALIZADA_QUEUE = "notificacao.cirurgia.atualizada.queue";
     public static final String CIRURGIA_CANCELADA_QUEUE = "notificacao.cirurgia.cancelada.queue";
+    
+    public static final String NOTIFICACAO_CIRURGIA_CRIADA_ROUTING_KEY = "notificacao.cirurgia.criada";
+    public static final String NOTIFICACAO_CIRURGIA_ATUALIZADA_ROUTING_KEY = "notificacao.cirurgia.atualizada";
+    public static final String NOTIFICACAO_CIRURGIA_CANCELADA_ROUTING_KEY = "notificacao.cirurgia.cancelada";
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Queue notificacaoCirurgiaCriadaQueue() {
+        return new Queue(CIRURGIA_CRIADA_QUEUE, true);
+    }
+
+    @Bean
+    public Binding notificacaoCirurgiaCriadaBinding() {
+        return BindingBuilder.bind(notificacaoCirurgiaCriadaQueue())
+                .to(exchange())
+                .with(NOTIFICACAO_CIRURGIA_CRIADA_ROUTING_KEY);
+    }
+
+    @Bean
+    public Queue notificacaoCirurgiaAtualizadaQueue() {
+        return new Queue(CIRURGIA_ATUALIZADA_QUEUE, true);
+    }
+
+    @Bean
+    public Binding notificacaoCirurgiaAtualizadaBinding() {
+        return BindingBuilder.bind(notificacaoCirurgiaAtualizadaQueue())
+                .to(exchange())
+                .with(NOTIFICACAO_CIRURGIA_ATUALIZADA_ROUTING_KEY);
+    }
+
+    @Bean
+    public Queue notificacaoCirurgiaCanceladaQueue() {
+        return new Queue(CIRURGIA_CANCELADA_QUEUE, true);
+    }
+
+    @Bean
+    public Binding notificacaoCirurgiaCanceladaBinding() {
+        return BindingBuilder.bind(notificacaoCirurgiaCanceladaQueue())
+                .to(exchange())
+                .with(NOTIFICACAO_CIRURGIA_CANCELADA_ROUTING_KEY);
+    }
 
     @Bean
     public MessageConverter messageConverter() {
